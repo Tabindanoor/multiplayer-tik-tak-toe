74d859246f0c7949b97924b4d0fab199471f6e95
Tabindanoor <tabindanoor415@gmail.com>
2024-07-07 13:24:44 +0500

    done

--- src/components/Waiting.tsx
+++ src/components/Waiting.tsx
@@ -1,41 +1,8 @@
-// import './Waiting.css'
-// const Waiting = () => {
-//   return (
-// <div> 
-//   <div className='ripple-background'>
-//       <div className='circle xxlarge shade1'></div>
-//       <div className='circle xlarge shade2'></div>
-//       <div className='circle large shade3'></div>
-//       <div className='circle medium shade4'></div>
-//       <div className='circle small shade5'></div>
-//   </div>
-//   <button className='bg-black text-white mx-auto '>Waiting for Opponent</button>
-    
-
-// </div>
-   
-
-//     // <>
-//     // <div className='circle xxlarge  shade1'></div>
-//     // <div className='circle xlarge shade2'></div>
-//     // <div className='circle large shade3'></div>
-//     // <div className='circle medium shade4'></div>
-//     // <div className='circle small shade5'></div>
-//     // </>
-  
-
-//   )
-// }
-
-// export default Waiting
-
-
-
 import './Waiting.css';
 
 const Waiting = () => {
   return (
-    <div className="ripple-background">
+    <div className="ripple-background h-screen">
       <div className="circle xxlarge shade1"></div>
       <div className="circle xlarge shade2"></div>
       <div className="circle large shade3"></div>
